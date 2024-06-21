64a3ced3c56b9c7e80e1866a127fbbf1f47e5ce4
Rabia Ghafoor <57128418+Rabia-Ghafoor@users.noreply.github.com>
2024-06-21 17:14:58 -0400

    changed in json script

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -7,13 +7,26 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import {initializeApp} from "firebase-admin/app";
 import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
+import {Firestore} from "firebase-admin/firestore";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+initializeApp();
+
+const firestore = new Firestore(); // firestore object
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`User created: ${JSON.stringify(userInfo)}`);
+  return;
+});
